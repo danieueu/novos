65c997eeca7f67a9e76040efba1fd7e61c8f54b1
danieueu <danifonso33gmail.com>
2025-02-27 03:20:03 +0000

    Super Feitos

---
+++ [lspd]/l_gallardo/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/contentunlocks.meta',
+	'data/**/handling.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/vehicles.meta',
+	'data/**/caraddonContentUnlocks.meta'
+}
+
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/caraddoncontentunlocks.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+
+
+files {
+	'audio/*.dat151.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/10lp570scpd_game.dat'
