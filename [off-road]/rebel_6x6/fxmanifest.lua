b702704e4e35de23f3823a247bf6b4137968033b
danieueu <danifonso33gmail.com>
2025-02-27 04:23:03 +0000

    Sports e Off-Road ficheiros adicionados

---
+++ [off-road]/rebel_6x6/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/contentunlocks.meta',
+	'data/**/handling.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/vehicles.meta',
+	'data/**/caraddonContentUnlocks.meta'
+}
+
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/caraddoncontentunlocks.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+
+files {
+	'audio/*.dat151.rel',
+	'audio/*.dat54.rel',
+	'audio/**/*.awc'
+  }
+  
+data_file 'AUDIO_GAMEDATA' 'audio/rebelc3_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/rebelc3_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/dlc_rebelc3'
